2d6a08d79d5956815f6d11255b9fc2155daa11f5
paaassscccaaalll <pphollenstein@gmail.com>
2025-06-03 15:38:59 +0200

    bugfix for handeling of refrences in forall

--- prusti-encoder/src/encoders/mir_pure.rs
+++ prusti-encoder/src/encoders/mir_pure.rs
@@ -1035,7 +1035,27 @@ impl<'vir: 'enc, 'enc> Enc<'vir, 'enc> {
                     .deps
                     .require_local::<RustTyCastersEnc<CastTypePure>>(closure_ty)
                     .unwrap();
-                reify_args.push(closure_ref);
+                
+                let has_ref_upvars = match closure_ty.kind() {
+                    TyKind::Closure(_, cl_args) => {
+                        cl_args.as_closure().upvar_tys().iter().any(|upvar_ty| {
+                            matches!(upvar_ty.kind(), ty::TyKind::Ref(_, _, _))
+                        })
+                    }
+                    _ => false,
+                };
+                if !(has_ref_upvars) {
+                    reify_args.push(
+                        cast.cast_to_concrete_if_possible(
+                            self.vcx,
+                            ref_to_closure_ty_out 
+                                .value_access
+                                .apply(self.vcx, [closure_ref]),
+                        ),
+                    );
+                } else {
+                    reify_args.push(closure_ref)
+                }
                 reify_args.extend(
                     qvars
                         .iter()
