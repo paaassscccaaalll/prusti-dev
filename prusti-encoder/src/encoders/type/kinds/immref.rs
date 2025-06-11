b5663260e269de1c45fb9310b23819f27f80d7b9
paaassscccaaalll <pphollenstein@gmail.com>
2025-06-11 14:10:19 +0200

    new axiom for immref

--- prusti-encoder/src/encoders/type/kinds/immref.rs
+++ prusti-encoder/src/encoders/type/kinds/immref.rs
@@ -44,6 +44,11 @@ pub(crate) fn domain<'vir>(
             {[output_ref.typeof_function]([cons_ident](r, p))}
             ([output_ref.typeof_function]([cons_ident](r, p))) == ([ty_type_func.ty_constructor]([generic_enc.param_type_function](p)))
     });
+    builder.axiom("value_typeof", vir::expr! {
+        forall s: [builder.self_type()] ::
+            {[generic_enc.param_type_function]([value_ident](s))}
+            ([generic_enc.param_type_function]([value_ident](s))) == ([output_ref.ty_param_accessors[0]]([output_ref.typeof_function](s)))
+    });
     // builder.axiom("cons", vir::expr! {
     //     forall s: [builder.self_type()] :: {[deref_ident](s)} ([cons_ident]([deref_ident](s))) == (s)
     // });
