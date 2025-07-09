192d95edf7db39aff593427c6c3917aed37fdb16
Pascal Hollenstein <phollenstein@student.ethz.ch>
2025-07-09 18:49:30 +0200

    continue in wand

--- prusti-encoder/src/encoders/impure/fn_wand.rs
+++ prusti-encoder/src/encoders/impure/fn_wand.rs
@@ -377,7 +377,9 @@ impl TaskEncoder for WandEnc {
                     todo!("region bound pair: {pred:?}");
                 };
                 let ty::RegionKind::ReEarlyParam(a) = pred.1.kind() else {
+                    continue;
                     todo!("region bound pair: {pred:?}");
+                    
                 };
                 insert_edge(IndirectKey::Early(a), IndirectKey::Param(b));
             }
